5e3b1309e9a040ff985fdd8cb5b89b56e4d6173e
sejoroajose <ajosesejoro@gmail.com>
2023-08-16 16:30:43 +0100

    Begining of the Functions project in C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - this is the manin function
+ * description - this function prints the word "_putchar"
+ */
+int main(void)
+
+{
+
+printf("_putchar\n")
+return(0)
+
+}
