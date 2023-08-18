2ae4083c88f21bee53c63e329d526dab535acf01
sejoroajose <ajosesejoro@gmail.com>
2023-08-18 06:54:10 +0100

    learning functions and loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  _isdigit - checks if argument is a digit
+ * @c : argument
+ * Return: 1 if c is a digit, return 0 if otherwise
+ */
+
+int  _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two arguments a and b
+ * @a: int to be multiplied by b
+ * @b: int to be multiplied by a
+ * Return: the result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
