1db86476bfc59193b7a18fd09a89c5afe7acd4fc
sejoroajose <ajosesejoro@gmail.com>
2023-09-02 03:34:09 +0100

    argv and argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ *main - prints program's name follwed by newline
+ *@argc: the c argument
+ *@argv: the v argument
+ *Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
