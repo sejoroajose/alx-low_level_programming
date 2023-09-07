bf5391e4c257206145181740ee3040f72e55c80a
sejoroajose <ajosesejoro@gmail.com>
2023-09-07 01:05:24 +0100

    Learning Memory Allocation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - function to create array of chars, and initializes
+ *  it with a specific char.
+ *  @c: the char to initialize
+ *  @size: number of bytes to allocate
+ *
+ *  Return: returns a pointer to the array or null if it fails
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *array = malloc(size);
+
+	if (size == 0 || array == 0)
+		return (NULL);
+	while (size--)
+		array[size] = c;
+	return (array);
+}i
