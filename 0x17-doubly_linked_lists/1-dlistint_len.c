784d1bd1745ba1f9f63b5fb686dc4d4d08c48bcd
sejoroajose <ajosesejoro@gmail.com>
2023-11-10 01:59:43 +0100

    more on linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Returns the number of elements in a dlistint_t list.
- * @h: Pointer to the head of the list.
- * Return: The number of nodes.
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of elements in the dlistint_t list.
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		count++;
+		nodes++;
 		h = h->next;
 	}
-	return (count);
+
+	return (nodes);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,8 @@
-#ifndef LIST_H
-#define LIST_H
-#include <stddef.h>
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -11,18 +11,18 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-size_t print_dlistint(const dlistint_t *h);
-size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
@@ -30,4 +30,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif
+#endif /* LISTS_H */
